a577ae6880c9fee38ffec8cfb51a683cbe4496e2
rohaizanr <rohaizanr@gmail.com>
2024-01-07 00:12:33 +0800

    new component

--- src/component/CowSay.js
+++ src/component/CowSay.js
@@ -1,9 +1,37 @@
+// useState  ... untuk declare variable
+// useEffect ... untuk memanggil function
+// useRef ... untuk bind ke satu variable component
+
+import React, { useState, useEffect } from 'react';
 
 const CowSay = () => {
-    const data = "Mooooooooooooo!!!!!!!"
- 
+    const [data , setData] = useState(1);
+    const [saySomethingElse, setSaySomethingElse] = useState();
+
+    const handleClick = () => {
+        setData('1');
+    }
+
+    useEffect(() => {
+        setData(1000);
+    }, [])
+
+    useEffect(() => {
+        function detectChanges() {
+            if(data === '1') {
+                setSaySomethingElse('Yoooooooo!');
+            }
+        }
+        detectChanges()
+    }, [data])
+
     return(
-        <>{data}</>
+        <>
+        {data}
+        <br />
+        <button onClick={handleClick}>Click</button>
+        <p>Say what? : {saySomethingElse}</p>
+        </>
     )
 }
 
